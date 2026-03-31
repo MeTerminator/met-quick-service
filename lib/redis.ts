97876db579fe205f230e4d73c99403db0c90f080
MeTerminator <meterminator666@gmail.com>
2026-03-31 12:20:44 +0800

    feat: add Analytics & SpeedInsights

--- lib/redis.ts
+++ lib/redis.ts
@@ -5,9 +5,9 @@ let client: ReturnType<typeof createClient> | null = null;
 export async function getRedisClient() {
   if (!client) {
     client = createClient({
-      url: process.env.REDIS_URL || 'redis://localhost:6379'
+      url: process.env.MET_REDIS_URL || 'redis://localhost:6379'
     });
-    
+
     client.on('error', (err) => console.log('Redis Client Error', err));
     await client.connect();
   } else if (!client.isOpen) {
